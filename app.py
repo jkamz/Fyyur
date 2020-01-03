26d59ee28844c3c00f4ef7ed77f5df7a4f8a2768
jkamz <jkamz6432@gmail.com>
2020-01-03 22:02:18 +0300

    add missing artist fields

--- app.py
+++ app.py
@@ -60,6 +60,9 @@ class Artist(db.Model):
     genres = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
+    website = db.Column(db.String)
+    seeking_venue = db.Column(db.Boolean(), nullable=False, default=False)
+    seeking_description = db.Column(db.String)
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
